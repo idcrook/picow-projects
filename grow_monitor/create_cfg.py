59b11993e9e6ded87cc7df9bdf25e78c9eda8094
David Crook <idcrook@users.noreply.github.com>
2024-01-28 12:14:10 -0700

    update config schema

--- grow_monitor/create_cfg.py
+++ grow_monitor/create_cfg.py
@@ -2,31 +2,31 @@
 
 import json
 
-example_config = { 
-  "i2c" :
-        { "bme280_address" : 0x77,
-          "ssd1306_address" : 0x3c,
-          "ssd1306_width" : 128,
-          "ssd1306_height" : 32,
-          "sda_pin" : 4,
-          "scl_pin": 5
-        }, 
- "onewire" :
-        { "data_pin" : 26
-        },         
-  "wifi" :
-        { "ssid" : "guest_net",
-           "password": "mypassword"
-        },
-  "global":
-        { "sensor_read_interval_seconds" : 30
-        },         
-  "mqtt":
-        { 
-          "broker" : "192.168.1.2",
-          "port" : 1883,
-          "topic_root" : "picow0"
-        }
+example_config = {
+    "i2c": {
+        "bme280_address": 0x77,
+        "ssd1306_address": 0x3c,
+        "ssd1306_width": 128,
+        "ssd1306_height": 32,
+        "sda_pin": 4,
+        "scl_pin": 5
+    },
+    "onewire": {
+        "data_pin": 26
+    },
+    "wifi": {
+        "ssid": "guest_net",
+        "password": "mypassword"
+    },
+    "global": {
+        "sensor_read_interval_seconds": 30,
+        "enable_bme": False
+     },
+    "mqtt": {
+        "broker": "192.168.1.2",
+        "port": 1883,
+        "topic_root": "picow0"
+    }
 }
 
 
