7b743faf7ac79f35a610efac8ff5a073641b367a
David Crook <idcrook@users.noreply.github.com>
2025-01-29 12:49:24 -0700

    add some network-related info

--- ha_temperature_discovery/mip_install.py
+++ ha_temperature_discovery/mip_install.py
@@ -1,6 +1,7 @@
 import errno
 import time
 import network
+import ubinascii
 
 import json
 import sys
@@ -9,13 +10,25 @@ import sys
 # from secrets import WIFI_SSID, WIFI_PASSWORD, MQTT_SERVER, MQTT_PORT, MQTT_USER, MQTT_PASSWORD
 from secrets import WIFI_SSID, WIFI_PASSWORD
 
-
 ssid = WIFI_SSID
 password = WIFI_PASSWORD
+hname = 'picow0'
 
+# https://docs.micropython.org/en/latest/library/network.html#network.hostname
+network.hostname(hname)
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(ssid, password)
+
+mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
+print(mac)
+
+### https://docs.micropython.org/en/latest/library/network.WLAN.html
+# Other things you can query
+print(wlan.config('hostname'))
+print(wlan.config('channel'))
+print(wlan.config('essid'))
+print(wlan.config('txpower'))
 
 # Wait for connect or fail
 max_wait = 10
@@ -42,7 +55,6 @@ mip.install("ssd1306")
 mip.install("onewire")
 mip.install("ds18x20")
 mip.install("github:peterhinch/micropython-mqtt", target="third-party")
-# do not use # mip.install("github:idcrook/micropython-tomli", target="third-party")
 #mip.install("github:idcrook/MicroPython_BMPxxx", target="third-party")
 mip.install("github:bradcar/MicroPython_BMPxxx", target="third-party")
 
